d15f42bfb8cc3125c8de63c9ab1d3309658ad983
Deployer <deployer@example.com>
2026-07-24 11:54:27 +0530

    Add /check-python debug route

--- routes/web.php
+++ routes/web.php
@@ -55,3 +55,13 @@ Route::get('/pip-install', function () {
     }
 });
 
+Route::get('/check-python', function () {
+    try {
+        $result = \Illuminate\Support\Facades\Process::run(['python3', '-c', 'import youtube_transcript_api; print(dir(youtube_transcript_api.YouTubeTranscriptApi))']);
+        return 'Python Output: <br><pre>' . $result->output() . '</pre><br>Error: <pre>' . $result->errorOutput() . '</pre>';
+    } catch (\Exception $e) {
+        return 'Error checking python: ' . $e->getMessage();
+    }
+});
+
+
